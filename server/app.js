f61c036b6704115145cf12ba757e078d6e32ccbc
Jules Perryman <jules.perryman@gmail.com>
2017-05-23 11:15:25 -0600

    nothing important

--- server/app.js
+++ server/app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const path = require('path')
 const app = express()
+// const pg = require('./db/knex')
+// const port = process.env.PORT || 3000
 
 
 app.use('/', express.static(path.join(__dirname, '../client')))
